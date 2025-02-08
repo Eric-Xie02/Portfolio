6afbc634358c3093aa94f4d7fb97f0dbfb9790e3
Eric Xie <exie102202@gmail.com>
2025-02-07 20:33:10 -0500

    update favicon

--- src/components/InitialsLogo.js
+++ src/components/InitialsLogo.js
@@ -1,8 +1,3 @@
-export default function InitialsLogo({
-  backgroundColor = "#fbb052", // Default background color (orange-ish)
-  textColor = "white", // Default text color (white)
-  initials = "EX", // Default initials
-  size = "50px", // Default size of the icon
-}) {
+export default function InitialsLogo({ initials = "EX" }) {
   return <div className="initials-logo">{initials}</div>;
 }
